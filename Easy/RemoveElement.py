f03408f46d874f21831d175fd6fd5e456090c715
nk0905 <0dd2n.a0905@gmail.com>
2024-09-25 22:32:23 +0900

    removeelement

---
+++ Easy/RemoveElement.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = len(nums)
+        i = 0
+        while i < k:
+            if nums[i] == val:
+                nums[i] = nums[k-1]
+                nums[k-1] = '_'
+                k -= 1
+                i -= 1
+            i += 1
+        return k
+        
