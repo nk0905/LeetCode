723e83968770c51673bd1ab6f4533113a82d1726
nk0905 <0dd2n.a0905@gmail.com>
2024-10-08 23:18:21 +0900

    fix

--- Easy/RemoveElement.py
+++ Easy/RemoveElement.py
@@ -1,5 +1,4 @@
 # https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
-
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         k = len(nums)
